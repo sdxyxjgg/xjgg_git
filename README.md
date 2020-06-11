494ea1b512f1c18b30f2bb4f4c77d3cc62cad208
xjgg <3250403060@qq.com>
2020-06-11 22:54:54 +0800

    git staly

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# xjgg_git
-git学习
+# git学习
+git学习，因为U盘毁了，把代码上传到这里为了防止丢失，实现了linux和windows服务器的git同步，需要的可以下载
---
+++ window同步.php
@@ -0,0 +1,17 @@
+<?php
+//自配window系统，需要先安装git软件
+$secret = "xjgg";
+$path = 'C:/Program Files/Git/bin';
+$signature = $_SERVER['HTTP_X_HUB_SIGNATURE'];
+
+if ($signature) {
+    $hash = "sha1=" . hash_hmac('sha1', file_get_contents("php://input"), $secret);
+    if (strcmp($signature, $hash) == 0) {
+        // echo shell_exec('cd {$path} && dir && git --version');
+        echo shell_exec("cd {$path} && git clone https://github.com/sdxyxjgg/fwqtest.git C:/wamp64/www/sync/xjgg");
+        // echo shell_exec("cd {$path} && git reset --hard origin/master && git clean -f && git pull 2>&1");
+        exit();
+    }
+}
+
+http_response_code(404);
